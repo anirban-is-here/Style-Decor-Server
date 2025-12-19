b208c34a05d937863ba9a75a3e458feb64632cb1
Anirban Mukherjee <anirbanmukherjee.cse@gmailcom>
2025-12-19 19:47:20 +0600

    FIxed bugs

--- src/app.js
+++ src/app.js
@@ -1,31 +1,16 @@
-import express from "express";
-import cors from "cors";
-import authRoutes from "./routes/auth.route.js";
-import bookingRoutes from "./routes/booking.route.js";
-import servicesRoutes from "./routes/services.route.js";
-import usersRoutes from "./routes/users.route.js";
-
-const app = express();
-
-
-const allowedOrigins = ["https://tyle-decor.web.app", "http://localhost:5173"];
+const allowedOrigins = [
+  "https://tyle-decor.web.app",
+  "http://localhost:5173",
+  "https://style-decor-server-anirban.vercel.app"
+];
 
 app.use(cors({
-    origin: allowedOrigins,
-    credentials: true,
-}))
-
-
-
-app.use(express.json());
-
-app.use("/api/bookings", bookingRoutes);
-app.use("/api/services", servicesRoutes);
-app.use("/api/users", usersRoutes);
-app.use("/api/auth", authRoutes);
-
-app.get("/", (req, res) => {
-  res.send("Backend working");
-});
-
-export default app;
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 connectDB().then(() => {
   app.listen(port, () => {
